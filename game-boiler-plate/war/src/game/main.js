fd5ef18db95ef23cdea859d70c7851839eaacd57
oli <oliver.stadie@gmail.com>
2014-05-25 23:00:47 +0200

    added asynchronous timers, repeating and one-shot

--- game-boiler-plate/war/src/game/main.js
+++ game-boiler-plate/war/src/game/main.js
@@ -5,10 +5,18 @@ game.module('game.main').require('engine.core', 'game.moduleRed',
 	      backgroundColor : 0xb9bec7,
 
 	      init : function() {
-		      var redDotObj = new RedDotClass(game.system.width / 2,
-		          game.system.height / 2);
+		      // add one central red dot after 3 seconds
+		      this.addTimer(3000, function() {
+			      var redDotObj = new RedDotClass(game.system.width / 2,
+			          game.system.height / 2);
+		      });
 
-		      var blueDotObj = new BlueDotClass(50, 200);
+		      // add one randomly positioned blue dot every 1 seconds
+		      this.addTimer(1000, function() {
+			      var rndX = Math.random() * game.system.width;
+			      var rndY = Math.random() * game.system.height;
+			      var blueDotObj = new BlueDotClass(rndX, rndY);
+		      }, true);
 	      }
 	    });
 
