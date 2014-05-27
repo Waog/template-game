b66c8b8b816cabfcc62ee1ce26c1527618628bdb
oli <oliver.stadie@gmail.com>
2014-05-27 21:26:31 +0200

    - Spritesheet integration - Util module

--- game-boiler-plate/war/src/game/moduleBlue.js
+++ game-boiler-plate/war/src/game/moduleBlue.js
@@ -1,23 +1,34 @@
-game.module('game.moduleBlue').body(function() {
+game.module('game.moduleBlue').require('utils.Util').body(
+    function() {
 
-	game.addAsset('blueDot.png');
+	    game.addAsset('blueDot.png');
+	    game.addAsset('spritesheet.json');
 
-	BlueDotClass = game.Class.extend({
-		init : function(x, y) {
-			this.sprite = new game.Sprite('blueDot.png');
-			this.sprite.anchor.set(0.5, 0.5);
-			this.sprite.position.set(x, y);
-			this.sprite.rotation = Math.random();
-			this.sprite.alpha = Math.random();
-			this.sprite.scale.x = Math.random();
-			this.sprite.scale.y = Math.random();
+	    BlueDotClass = game.Class.extend({
+	      init : function(x, y) {
 
-			// var width = this.sprite.width;
-			// var height = this.sprite.height;
-			// console.log('blueDot - width: ', width, 'height', height);
+		      this.sprite = this.getRandomSprite();
+		      this.sprite.anchor.set(0.5, 0.5);
+		      this.sprite.position.set(x, y);
+		      this.sprite.rotation = Math.random();
+		      this.sprite.alpha = Math.random();
+		      this.sprite.scale.x = Math.random();
+		      this.sprite.scale.y = Math.random();
 
-			game.scene.stage.addChild(this.sprite);
-		},
-	});
+		      // var width = this.sprite.width;
+		      // var height = this.sprite.height;
+		      // console.log('blueDot - width: ', width, 'height', height);
 
-});
+		      game.scene.stage.addChild(this.sprite);
+	      },
+
+	      getRandomSprite : function() {
+	      	// from sprite sheet
+		      var fileName = new Util().getRandomArrayElement([ 'blueCross.png',
+		          'blueSquare.png', 'blueStar.png' ]);
+
+		      return new game.Sprite(fileName);
+	      }
+	    });
+
+    });
